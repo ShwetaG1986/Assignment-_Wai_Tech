1c5a2da927c05dc9f879410007b370bc5c002421
laptop 1 <laptop 1@LAPTOP1-PC>
2020-06-09 17:51:50 +0530

    Add project files.

---
+++ Assignment _Wai_Tech/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment__Wai_Tech
+{
+   public class Movie
+    {
+        public string Title { get; set; }
+        public int  Rating { get; set; }
+
+
+        public Movie (string title, int rating)
+        {
+            Title = title;
+            Rating = rating;
+        }
+    }
+}
---
+++ Assignment _Wai_Tech/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment__Wai_Tech
+{
+    class Program
+    {
+       
+       
+       
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Please enter a Movie");
+                list.Add(Console.ReadLine()); // HERE !!
+            }
+        }
+
+        public static void GeteMovieNames(List<string> movie, List<int> rating)
+        {
+
+            for(int i = 0; i<movie.Count; i++)
+            {
+                Console.WriteLine("{0}\t\t\t{1}", movie[i], rating[i]);
+
+            }
+
+        }
+      
+
+
+
+
+}
+
+    
+}
